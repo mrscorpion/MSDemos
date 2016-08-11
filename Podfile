7c287d3edcf804e4e4e46a45556313db2e578893
mr.scorpion <mrscorpion1114@gmail.com>
2016-08-11 13:12:51 +0800

    add CocoaPod

---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, '7.0'
+target "MSDemos" do
+pod 'EZAudio'
+
+
+end
---
+++ Pods/Target Support Files/Pods-MSDemos/Pods-MSDemos-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MSDemos : NSObject
+@end
+@implementation PodsDummy_Pods_MSDemos
+@end
